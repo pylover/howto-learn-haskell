2960381befbb6047b9027c85b071a39d2b9eb4c5
pylover <vahid.mardani@gmail.com>
2024-02-18 23:53:54 +0330

    Monad Fail

--- monadfail.hs
+++ monadfail.hs
@@ -4,20 +4,48 @@ module Main where
 import Control.Monad
 
 
-foo :: Maybe Int
--- foo = Just 2
-foo = Nothing
+data Perhaps a 
+  = Only a 
+  | None
+  deriving (Show, Eq)
 
 
-bar :: Int -> Maybe String
-bar x = Just . show $ x
+instance Functor Perhaps where
+  fmap _ None = None
+  fmap f (Only x) = Only $ f x
 
 
-baz :: Maybe String
-baz = do
-  ~x <- foo
+instance Applicative Perhaps where
+  pure = Only   
+  None <*> _ = None
+  (Only f) <*> x = fmap f x 
+
+
+instance Monad Perhaps where
+  None >>= _ = None
+  (Only x) >>= k = k x
+
+
+instance MonadFail Perhaps where
+  fail _ = None
+
+
+foo :: Int -> Perhaps (Maybe Int)
+foo 0 = Only $ Nothing 
+foo x = Only $ Just x
+
+
+bar :: Int -> Perhaps (Maybe String)
+bar x = Only . Just . show $ x
+
+
+baz :: Int -> Perhaps (Maybe String)
+baz x = do
+  (Just x) <- foo x
   bar x
+
 
 main :: IO ()
 main = do
-  putStrLn $ "Hello: " ++ show baz
+  putStrLn $ "baz 0: " ++ show (baz 0)
+  putStrLn $ "baz 1: " ++ show (baz 1)
